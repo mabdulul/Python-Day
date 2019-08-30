6c1056ab14ccc357f2a44f5f062a69cce34ed8e4
Mulk Abdulhadi <mabdulul@gmail.com>
2019-08-30 16:04:52 -0400

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,12 @@
+#print("Hello World")
+#message = print( "Please fill in the blank:  [your name] favorite subject in school is [your favorite subject] ")
+#name = input("What is your name? ")
+##subject = input("What is subject? ")
+#message = f"{name}'s favorite subject in school in school is {subject}"https://learngitbranching.js.org/
+#print(message);
+
+
+user_input = int(input("Enter a number: "))
+print(type(user_input))
+result = user_input * user_input
+print(result).
